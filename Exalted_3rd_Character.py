83c2b3514de9980a203342511c9b832680d051a4
Exalted3rd <exalted3rd@none>
2021-02-23 13:54:50 -0700

    Adding Dice function and reorganzing the code

--- Exalted_3rd_Character.py
+++ Exalted_3rd_Character.py
@@ -26,20 +26,39 @@ class CharacterPrompt(cmd.Cmd):
     def do_shortcharacter(self, inp):
         print(str(TheCharacter))
 
+    #Top Level Statistics
+    def do_name(self, inp):
+        print(yaml.dump(TheCharacter.get("name"), default_flow_style=False))
     def do_essence(self, inp):
         print(yaml.dump(TheCharacter.get("essence"), default_flow_style=False))
     def do_willpower(self, inp):
         print(yaml.dump(TheCharacter.get("willpower"), default_flow_style=False))
+
+    #Attributes commands
+    def do_attributes(self, inp):
+        print(yaml.dump(TheCharacter.get("attributes"), default_flow_style=False))
 
     #Abilities commands
     def do_abilities(self, inp):
         print(yaml.dump(TheCharacter.get("abilities"), default_flow_style=False))
     def do_shortabilities(self, inp):
         print(str(TheCharacter.get("abilities")))
+    def do_ability(self, arg):
+        #'Save future commands to filename:  RECORD rose.cmd'
+        #self.file = open(arg, 'w')
+        if arg == "":
+            print("need an ability argument to print a value")
+            print("or did you mean abilities")
+            return
+        print(TheCharacter.get("abilities")[0][arg])
+        #arg)
 
-    #Attributes commands
-    def do_attributes(self, inp):
-        print(yaml.dump(TheCharacter.get("attributes"), default_flow_style=False))
+    def do_dice(self, arg):
+        print("arg " + arg)
+        print(type(arg))
+        for t_key in TheCharacter:
+            print(TheCharacter.get(t_key))
+            print(type(TheCharacter.get(t_key)))
 
     def help_character(self):
         print("Print the entire character statistics")
@@ -51,6 +70,10 @@ class CharacterPrompt(cmd.Cmd):
         print("Prints a condensed list of Character abilities")
     def help_attributes(self):
         print("Prints list of character Attributes")
+
+def parse(arg):
+    'Convert a series of zero or more numbers to an argument tuple'
+    return tuple(map(int, arg.split()))
 
 
 with open('Character.json') as f:
